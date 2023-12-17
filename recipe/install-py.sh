c70e7a3622568713898bc9ad4b62fecdccbaa054
Uwe L. Korn <uwelk@xhochy.com>
2023-12-17 08:58:38 +0100

    Set empty default for CROSSCOMPILING_EMULATOR

--- recipe/install-py.sh
+++ recipe/install-py.sh
@@ -6,7 +6,7 @@ pushd bindings/python
 make install3
 make install3_cython
 if [[ "$(python -c 'import sys; print(sys.implementation.name)')" == "cpython" ]]; then
-  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
     make check
   fi
 fi
