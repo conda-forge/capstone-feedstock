e467b20e27666870f799bd86415c48707430e1c4
Joris Vaillant <joris.vaillant@inria.fr>
2025-06-11 14:50:36 +0200

    Activate libcapstone Windows build

---
+++ recipe/install-lib.bat
@@ -0,0 +1,21 @@
+@echo on
+
+if "%PKG_NAME%"=="cstool" (
+  set CAPSTONE_BUILD_CSTOOL=ON
+)
+else (
+  set CAPSTONE_BUILD_CSTOOL=OFF
+)
+
+cmake %CMAKE_ARGS% ^
+  -B build ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCAPSTONE_BUILD_SHARED_LIBS=1 ^
+  -DCAPSTONE_BUILD_CSTOOL=%CAPSTONE_BUILD_CSTOOL%
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --build build -j%CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --install build
+if %ERRORLEVEL% neq 0 exit 1
