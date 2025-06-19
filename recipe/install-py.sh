56549d7c12a2887140d92e65c111f6e0e50fb183
Joris Vaillant <joris.vaillant@gmail.com>
2025-06-19 10:54:31 +0200

    Update recipe/install-py.sh
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipe/install-py.sh
+++ recipe/install-py.sh
@@ -3,7 +3,7 @@
 set -exuo pipefail
 
 pushd bindings/python
-$PYTHON -m pip install . -vv
+$PYTHON -m pip install --no-deps --no-build-isolation . -vv
 if [[ "$($PYTHON -c 'import sys; print(sys.implementation.name)')" == "cpython" ]]; then
   if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
     $PYTHON tests/test_all.py
