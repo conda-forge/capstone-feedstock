2d43a825cb09e436b93c18727e122daad245ee54
H. Vetinari <h.vetinari@gmx.com>
2025-12-25 15:45:40 +1100

    move python tests to recipe

--- recipe/install-py.sh
+++ recipe/install-py.sh
@@ -4,10 +4,5 @@ set -exuo pipefail
 
 pushd bindings/python
 make install3
-if [[ "$(python -c 'import sys; print(sys.implementation.name)')" == "cpython" ]]; then
-  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-    python tests/test_all.py
-  fi
-fi
 popd
 
